d9a8c2257410bd7b228aec258bc08abac5f05c64
prem <premkumarv08@gmail.com>
2025-01-16 06:35:57 +0530

    Exploring different HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
+
+app.get("/user", (req, res) => {
+    res.send({"firstname": "prem", "lastname": "kumar"})
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data added to database successfully")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Data removed from database successfully")
+})
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test");
+});
 
 app.use("/", (req, res) => {
-    res.send("Hello from dashboard")
-})
-app.use("/test", (req, res) => {
-    res.send("Hello from test")
-})
+  res.send("Hello from dashboard");
+});
+
 app.listen(3000, () => {
-    console.log("Server started")
-})
+  console.log("Server started");
+});
