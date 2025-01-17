91097b5ffc4885c020253ad137b3cc303feb8ec1
prem <premkumarv08@gmail.com>
2025-01-17 07:47:40 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({"firstname": "prem", "lastname": "kumar"})
-})
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("1st route");
+    res.send({ firstname: "prem", lastname: "kumar" });
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("2nd route");
+      next();
+    },
+    (req, res, next) => {
+      console.log("3rd route");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("4th route");
+    next();
+  },
+  (req, res) => {
+    console.log("5th route");
+  }
+);
 
 app.post("/user", (req, res) => {
-    res.send("Data added to database successfully")
-})
+  res.send("Data added to database successfully");
+});
 
 app.delete("/user", (req, res) => {
-    res.send("Data removed from database successfully")
-})
+  res.send("Data removed from database successfully");
+});
 
 app.use("/test", (req, res) => {
   res.send("Hello from test");
