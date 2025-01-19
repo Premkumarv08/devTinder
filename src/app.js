53e126e4328359c19cd3dd58a4c803250dacc7a0
prem <premkumarv08@gmail.com>
2025-01-19 15:36:20 +0530

    PATCH api

--- src/app.js
+++ src/app.js
@@ -43,6 +43,26 @@ app.get('/feed', async (req, res) => {
   }
 })
 
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId)
+    res.send("User deleted successfully!")
+  } catch (error) {
+    res.status(400).send("Error while deleting the user:" + error.message);
+  }
+})
+
+app.patch('/user', async (req, res) => {
+  const {userId, ...data} = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {returnDocument: "after"})
+    res.send(user)
+  } catch (error) {
+    res.status(400).send("Error while updating the user:" + error.message);
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Database connection established");
