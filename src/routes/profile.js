5acf56d41f6b2cf93d7cab70ffa2849e4c7b30c6
prem <premkumarv08@gmail.com>
2025-01-30 04:42:27 +0530

    Change password implementation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 const { authenticateUser } = require("../middlewares/auth");
 const { validateEditData } = require("../utils/validations");
 const profileRouter = express.Router();
@@ -30,5 +32,34 @@ profileRouter.patch("/profile/edit", authenticateUser, async (req, res) => {
     res.status(400).send("Error : " + error.message);
   }
 });
+
+profileRouter.patch(
+  "/profile/change-password",
+  authenticateUser,
+  async (req, res) => {
+    try {
+      const { user } = req;
+      const { currentPassword, newPassword, emailId } = req.body;
+      if (emailId !== user.emailId) {
+        throw new Error("Invalid Crendentials!");
+      }
+      const isPasswordValid = await user.validatePassword(currentPassword);
+      if (!isPasswordValid) {
+        throw new Error("Invalid Crendentials!");
+      }
+      if (!validator.isStrongPassword(newPassword))
+          throw new Error("Please enter a strong password!");
+      const passwordHash = await bcrypt.hash(newPassword, 10);
+      user.password = passwordHash;
+      await user.save();
+      res.json({
+        message: `${user.firstName}, your password has been updated successfully!`,
+        data: user,
+      });
+    } catch (error) {
+      res.status(400).send("Error : " + error.message);
+    }
+  }
+);
 
 module.exports = profileRouter;
